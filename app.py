14466fffcbe27dc29c39d49fc18b0f754ba372e3
Vanya88 <vanyachen88@gmail.com>
2026-09-23 15:19:11 +0800

    delete requirements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from analytics.db import read_sql
 
 
-
+st.title("Rental Business Analytics Dashboard - DEPLOY TEST")
 # ============================================================
 # PAGE CONFIG
 # ============================================================
